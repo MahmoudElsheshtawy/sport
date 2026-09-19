eedf74a29136599629fb90e6ecbc01eef606597c
mahmoud <MahmoudElsheshtawy@users.noreply.github.com>
2026-09-19 15:32:28 +0300

    dd

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -5,7 +5,7 @@ const connectDB = async ()=>{
         console.log(" DB connected " );
         
     })
-   await mongoose.connect(`${process.env.MONGODB_URL}/malabs`)
+   await mongoose.connect(`${process.env.MONGODB_URL}/sport`)
 
 
 
